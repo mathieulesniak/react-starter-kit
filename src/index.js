9f7901392eb221f3942dd3584c05ff9e0b1e48db
Mathieu LESNIAK <maverick@eskuel.net>
2016-06-18 19:44:21 +0200

    app entry

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+
+import configureStore from './stores/configureStore';
+
+const store = configureStore();
+
+render(
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>, document.getElementById('app')
+);
+
