eb36865f677be3590ca6b11d6e7ca0b8b7ba99ff
Mathieu LESNIAK <maverick@eskuel.net>
2016-06-18 20:31:12 +0200

    fix typo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 
+import routes from './routes';
 import configureStore from './store';
 
 const store = configureStore();
