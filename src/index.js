8b70c8e8c285539c08408edeef557d7a7cec1e3a
Mathieu LESNIAK <maverick@eskuel.net>
2016-06-18 20:15:00 +0200

    fix path

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 
-import configureStore from './store/configureStore';
+import configureStore from './store';
 
 const store = configureStore();
 
