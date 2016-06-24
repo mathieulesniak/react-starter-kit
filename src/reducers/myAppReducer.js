8103b6ccdc5594b3e29f08e4a1a70c2a44c09684
Mathieu LESNIAK <maverick@eskuel.net>
2016-06-24 22:43:24 +0200

    fix listing issues

--- src/reducers/myAppReducer.js
+++ src/reducers/myAppReducer.js
@@ -8,13 +8,13 @@ export default function myAppReducer(state = initialState, action) {
   switch (action.type) {
     case types.INCREASE_COUNTER:
       return { ...state, counter: state.counter + 1 };
-    
+
     case types.DECREASE_COUNTER:
       return { ...state, counter: state.counter - 1 };
-    
+
     case types.RESET_COUNTER:
       return { ...state, counter: 1 };
-    
+
     default:
       return state;
   }
